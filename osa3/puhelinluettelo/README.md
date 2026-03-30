7a4174aecaea66edd5b432e94e5b2854d95813a1
a-terho <140437240+a-terho@users.noreply.github.com>
2026-03-30 13:22:14 +0300

    linkki

--- osa3/puhelinluettelo/README.md
+++ osa3/puhelinluettelo/README.md
@@ -1,3 +1,3 @@
 # puhelinluettelo
 
-Tehtävät 3.1 - 3.11
+Tehtävät 3.1 - 3.11. Linkki verkkosovellukseen: https://puhelinluettelo-i36h.onrender.com/
--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -114,7 +114,7 @@ app.delete('/api/persons/:id', (req, res) => {
 });
 
 // luo palvelin
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
