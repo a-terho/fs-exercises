a08e04468f5b117714a252fd6363edb095ba73c5
a-terho <140437240+a-terho@users.noreply.github.com>
2026-05-11 14:08:14 +0000

    osa11 11.9

--- osa11/fs-pokedex/e2e-tests/example.spec.js
+++ osa11/fs-pokedex/e2e-tests/example.spec.js
@@ -1,13 +1,25 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
         'Pokémon and Pokémon character names are trademarks of Nintendo.',
       ),
     ).toBeVisible()
+  })
+
+  test('you can navigate to a Pokemon specific page by clicking one', async ({
+    page,
+  }) => {
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('Special Defense')).toBeVisible()
+    await expect(page.getByText('Special Attack')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
