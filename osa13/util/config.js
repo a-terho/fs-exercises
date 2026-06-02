f308e52af88265dfeb30e9793a360a8888d873e1
a-terho <140437240+a-terho@users.noreply.github.com>
2026-06-02 12:11:55 +0000

    osa13 13.17 fixes

--- osa13/util/config.js
+++ osa13/util/config.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
-const DATABASE_URL = process.env.TEST_DATABASE_URL || process.env.DATABASE_URL;
+const DATABASE_URL = process.env.TESTING
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL;
 const SECRET = process.env.SECRET;
 
 module.exports = { PORT, DATABASE_URL, SECRET };
