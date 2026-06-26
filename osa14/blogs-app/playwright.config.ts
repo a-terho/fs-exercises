3e28e732a3d8560bb3dd86018c13cf8b9d1ac109
a-terho <140437240+a-terho@users.noreply.github.com>
2026-06-26 17:50:11 +0000

    osa14 more test server setup

--- osa14/blogs-app/playwright.config.ts
+++ osa14/blogs-app/playwright.config.ts
@@ -7,5 +7,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run test:dev',
     url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 });
